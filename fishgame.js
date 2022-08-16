69bf340b667a173dfdf1d9a8c4788023a4e4d1f4
Mahak Jain <64734114+mahakjain026@users.noreply.github.com>
2022-08-16 18:22:36 +0530

    Update fishgame.js

--- fishgame.js
+++ fishgame.js
@@ -5,27 +5,54 @@ let garbages;
 let cord;
 let timer;
 
-document.addEventListener("mousemove", movement);
-function getScoreFromLS(){
-  
-  return JSON.parse(localStorage.getItem('scoreObj'));
+document.addEventListener("mousemove", movement1);
+document.addEventListener("touchmove", movement);
 
+// function isTouchDevice(){
+//   return true == ("ontouchstart" in window || window.DocumentTouch && document instanceof DocumentTouch);
+// }
+
+// if(isTouchDevice()===true) {
+//   alert('Touch Device'); //your logic for touch device
+// }
+// else {
+//   // alert('Not a Touch Device'); //your logic for non touch device
+// }
+
+function getScoreFromLS() {
+  return JSON.parse(localStorage.getItem("scoreObj"));
 }
 
-function setScoreFromLS(scorePara){
-  console.log("--------in the set")
+function setScoreFromLS(scorePara) {
+  // console.log("--------in the set")
   let currentScore = getScoreFromLS();
-  if(!currentScore){
+  if (!currentScore) {
     currentScore = {
-      "highscore" : 0,
-      "score" : 0
+      highscore: 0,
+      score: 0,
     };
   }
-  currentScore["highscore"] = currentScore["highscore"]<scorePara ? scorePara : currentScore["highscore"];
+  currentScore["highscore"] =
+    currentScore["highscore"] < scorePara
+      ? scorePara
+      : currentScore["highscore"];
   currentScore["score"] = scorePara;
-  localStorage.setItem('scoreObj', JSON.stringify(currentScore));
+  localStorage.setItem("scoreObj", JSON.stringify(currentScore));
 }
+
 function movement(e) {
+  var x = e.touches[0].clientX;
+  var y = e.touches[0].clientY;
+
+  if (y < 80 || y > 430 || x < 156 || x > 750) {
+    return;
+  }
+
+  fishmouse.style.left = x + "px";
+  fishmouse.style.top = y + "px";
+}
+
+function movement1(e) {
   var x = e.clientX;
   var y = e.clientY;
 
@@ -40,6 +67,9 @@ function movement(e) {
 const particles = document.getElementById("particle");
 
 function randomImage() {
+if(cord &&cord.left > 150){
+  return
+}
   particles.innerHTML = "";
   const img = [
     "chips-removebg-preview.png",
@@ -56,11 +86,10 @@ function randomImage() {
   particles.appendChild(i);
 }
 
-function getImagePosition() {
+function getImagePosition(n) {
   const pos = Math.floor(Math.random() * 360) + 200;
-  console.log("pos", pos);
+  // console.log("pos", pos);
   document.getElementById("garbage").style.marginTop = pos + "px";
-  // console.log("margin---",document.getElementById("garbage").style.marginTop)
 }
 
 function isCollide() {
@@ -88,76 +117,95 @@ function calScore() {
   score = score + 5;
   setScoreFromLS(score);
   updateScore.innerText = "score : " + score;
-
-  // console.log("score");
 }
-
-
 
 function game() {
   if (isCollide()) {
     gameOver();
   }
-  
 }
-let claScoreInteval
-let randomImgInteval
-let gameInteval
-let imgPosInteval
+let claScoreInterval;
+let randomImgInterval;
+let gameInterval;
+let imgPosInterval;
+let changespeedInterval;
 
-function startGame() {
-  claScoreInteval = setInterval(calScore, 2000);
-
-  gameInteval = setInterval(game, 300);
-
-  randomImgInteval = setInterval(randomImage, 2000);
-
-  imgPosInteval = setInterval(getImagePosition, 2000);
+function clearGame() {
+  clearInterval(gameInterval);
+  clearInterval(claScoreInterval);
+  clearInterval(randomImgInterval);
+  clearInterval(imgPosInterval);
+  clearInterval(changespeedInterval);
 }
 
-function clearGame(){
-  console.log("in clear")
-  clearInterval(gameInteval)
-  clearInterval(claScoreInteval)
-  clearInterval(randomImgInteval)
-  clearInterval(imgPosInteval)
-}
 function playAgain() {
   window.location.href = "/index.html";
 }
 
-
-function gameOver(isOver){
-  clearGame()
-  console.log("score==",score);
-  window.location.href="/index3.html";
-   
+function replay() {
+  window.location.href = "/index2.html";
 }
 
-function showScore(){
-  let finalScoreValue = getScoreFromLS()
+function gameOver(isOver) {
+  clearGame();
+  window.location.href = "/index3.html";
+}
+
+function showScore() {
+  let finalScoreValue = getScoreFromLS();
   let gameOverScore = finalScoreValue["score"];
-  let gameOverHighScore = finalScoreValue["highscore"]
-  let finalHighScore=document.querySelector(".hs");
-  finalHighScore.innerHTML=`highscore : ${gameOverHighScore}`;
-  
-  let finalscore=document.querySelector(".sc");
-  finalscore.innerHTML=`Score : ${gameOverScore}`;
+  let gameOverHighScore = finalScoreValue["highscore"];
+  let finalHighScore = document.querySelector(".hs");
+  finalHighScore.innerHTML = `highscore : ${gameOverHighScore}`;
+  let finalscore = document.querySelector(".sc");
+  finalscore.innerHTML = `Score : ${gameOverScore}`;
 }
 
- function changespeed()
-  {
- if(a<score && score<a+40 && t> 0.5)
- 
-{
-a=a+40;
-t=t-0.2;
-t2=t2-200;
-console.log("a----",a);
- console.log("t------",t);
- document.getElementById("particle").style.animationDuration = `${t}s`;
-}
-//  $('#particle').css("animation-duration", "5s");
+var a = 20;
+var t = 4;
+var t2 = 4000;
 
+function changespeed() {
+  if (a >= 20 && t > 1 && t2 > 1000 ) {
+
+      console.log("N----");
+
+    a = a + 20;
+    let elem = document.getElementById("particle");
+    t = t - 1;
+    t = parseFloat(t).toFixed(2);
+    t2 = t2 - 1000;
+   elem.style.animationDuration = `${t}s`;
+    updateGameData();
+  }
 }
 
+function startGame() {
+  document.getElementById("particle").style.animationDuration = `4s`;
+  gameInterval = setInterval(game, 300);
+  claScoreInterval = setInterval(() => {
+    calScore();
+    randomImage();
+    getImagePosition();
+    changespeed();
+  }, t2);
+}
+
+function updateGameData() {
+  clearInterval(claScoreInterval);
+  // clearInterval(randomImgInterval);
+  // clearInterval(imgPosInterval);
+  // clearInterval(changespeedInterval);
+  claScoreInterval = setInterval(() => {
+    calScore();
+    randomImage();
+    getImagePosition();
+    changespeed();
+  }, t2);
+
+  // randomImgInterval = setInterval(randomImage, t2);
+
+  // imgPosInterval = setInterval(getImagePosition, t2);
+
+  // changespeedInterval = setInterval(changespeed,t2);
+}
